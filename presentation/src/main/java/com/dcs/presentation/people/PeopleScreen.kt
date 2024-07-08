537dea26185e610c3e4b8dbd51f94ef4fd16b4d8
jay.lee <stkment@gmail.com>
2024-07-08 10:58:53 +0900

    feat #2 각 화면 Screen 생성

---
+++ presentation/src/main/java/com/dcs/presentation/people/PeopleScreen.kt
@@ -0,0 +1,11 @@
+package com.dcs.presentation.people
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun PeopleScreen(
+    modifier: Modifier = Modifier,
+) {
+
+}
