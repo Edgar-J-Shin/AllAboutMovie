9f7383d7151c67fe75df58a8484435a470871765
jay.lee <stkment@gmail.com>
2024-07-08 23:01:02 +0900

    feat #2 MainApp 함수 이름 변경 및 리택토링

--- presentation/src/main/java/com/dcs/presentation/main/MainActivity.kt
+++ presentation/src/main/java/com/dcs/presentation/main/MainActivity.kt
@@ -4,10 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.dcs.presentation.theme.AllAboutMovieTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,27 +14,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AllAboutMovieTheme {
-                MainNavHost()
+                MainApp()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(
-    name: String,
-    modifier: Modifier = Modifier,
-) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AllAboutMovieTheme {
-        Greeting("Android")
     }
 }
---
+++ presentation/src/main/java/com/dcs/presentation/main/MainApp.kt
@@ -0,0 +1,67 @@
+package com.dcs.presentation.main
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.dcs.presentation.home.HomeScreen
+import com.dcs.presentation.main.Screen.Main
+import com.dcs.presentation.people.PeopleScreen
+import com.dcs.presentation.search.SearchScreen
+import com.dcs.presentation.setting.SettingScreen
+
+@Composable
+fun MainApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier,
+) {
+
+    Scaffold(
+        modifier = modifier.fillMaxSize(),
+        bottomBar = {
+            MainBottomNavigation(navController = navController)
+        }
+    ) { paddingValues ->
+        Box(modifier = Modifier.padding(paddingValues)) {
+            MainNavHost(
+                navController = navController, startDestination = Main.Home.route
+            )
+        }
+    }
+}
+
+@Composable
+private fun MainNavHost(
+    navController: NavHostController,
+    startDestination: String,
+    modifier: Modifier = Modifier,
+) {
+
+    NavHost(
+        modifier = modifier,
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        composable(route = Main.Home.route) {
+            HomeScreen()
+        }
+
+        composable(route = Main.Search.route) {
+            SearchScreen()
+        }
+
+        composable(route = Main.People.route) {
+            PeopleScreen()
+        }
+
+        composable(route = Main.Setting.route) {
+            SettingScreen()
+        }
+    }
+}
