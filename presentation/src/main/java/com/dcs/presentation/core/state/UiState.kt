65551ad9e65169b3833ec4204bbb6d54cd5db9b0
edgarshin <edgar.shin@tosslab.com>
2024-07-14 23:16:58 +0900

    feat #1 UiState 변경 - UiState>Failure 제거 - movie item dimens 값 추가

--- presentation/src/main/java/com/dcs/presentation/core/state/UiState.kt
+++ presentation/src/main/java/com/dcs/presentation/core/state/UiState.kt
@@ -12,7 +12,6 @@ import java.io.IOException
 
 sealed class UiState<out R> {
     data class Success<out T>(val data: T?) : UiState<T>()
-    data class Failure(val throwable: Throwable) : UiState<Nothing>()
     data object Loading : UiState<Nothing>()
     data class Error(val throwable: Throwable) : UiState<Nothing>()
 }
