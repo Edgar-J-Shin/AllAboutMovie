51eb7229aaa515e7a35a47a6c5431fd325e034f6
jay.lee <stkment@gmail.com>
2024-07-15 22:14:49 +0900

    feat #4 add SignIn button

--- presentation/src/main/java/com/dcs/presentation/setting/SettingScreen.kt
+++ presentation/src/main/java/com/dcs/presentation/setting/SettingScreen.kt
@@ -1,7 +1,16 @@
 package com.dcs.presentation.setting
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
@@ -9,5 +18,45 @@ fun SettingRoute(
     modifier: Modifier = Modifier,
     viewModel: SettingViewModel = hiltViewModel(),
 ) {
+    SettingScreen(
+        modifier = modifier.fillMaxSize(),
+    )
+}
 
+@Composable
+fun SettingScreen(
+    modifier: Modifier,
+) {
+    Column(
+        modifier = modifier,
+    ) {
+        SignInButton(
+            Modifier.fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+private fun SignInButton(
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Button(
+            shape = RoundedCornerShape(8.dp),
+            onClick = { /*TODO*/ }
+        ) {
+            Text(text = "로그인 하기")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewSettingScreen() {
+    SettingScreen(
+        modifier = Modifier.fillMaxSize(),
+    )
 }
