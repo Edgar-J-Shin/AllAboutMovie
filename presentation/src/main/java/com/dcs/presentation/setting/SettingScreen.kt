6eb1d48035f9608bab93ca7658972a0907359234
jay.lee <stkment@gmail.com>
2024-07-15 21:55:59 +0900

    feat #4 create SettingViewModel

--- presentation/src/main/java/com/dcs/presentation/setting/SettingScreen.kt
+++ presentation/src/main/java/com/dcs/presentation/setting/SettingScreen.kt
@@ -2,10 +2,12 @@ package com.dcs.presentation.setting
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
 fun SettingRoute(
     modifier: Modifier = Modifier,
+    viewModel: SettingViewModel = hiltViewModel(),
 ) {
 
 }
