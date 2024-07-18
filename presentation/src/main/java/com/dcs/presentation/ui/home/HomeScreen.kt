4a8119e90b27218b02dd33cf12b45411c98c21c6
edgarshin <edgar.shin@tosslab.com>
2024-07-18 16:23:49 +0900

    feat #7 Searchbar ui 구현

--- presentation/src/main/java/com/dcs/presentation/ui/home/HomeScreen.kt
+++ presentation/src/main/java/com/dcs/presentation/ui/home/HomeScreen.kt
@@ -1,11 +1,33 @@
 package com.dcs.presentation.ui.home
 
+import android.view.ViewTreeObserver
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.res.stringResource
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.dcs.presentation.R
+import kotlinx.coroutines.flow.StateFlow
 
 @Composable
 fun HomeRoute(
@@ -15,7 +37,11 @@ fun HomeRoute(
 
     Scaffold(
         topBar = {
-
+            SearchTopBar(
+                viewModel.queryText,
+                viewModel::onSearchTextChange,
+                viewModel::onSearchTextClear
+            )
         }
     ) { innerPadding ->
         Box(
@@ -25,3 +51,57 @@ fun HomeRoute(
         }
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun SearchTopBar(
+    queryText: StateFlow<String>,
+    onQueryChange: (String) -> Unit,
+    onQueryClear: () -> Unit
+) {
+    val searchText by queryText.collectAsStateWithLifecycle()
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val focusManager = LocalFocusManager.current
+
+    val view = LocalView.current
+    val viewTreeObserver = view.viewTreeObserver
+    DisposableEffect(viewTreeObserver) {
+        val listener = ViewTreeObserver.OnGlobalLayoutListener {
+            val isKeyboardOpen = ViewCompat.getRootWindowInsets(view)
+                ?.isVisible(WindowInsetsCompat.Type.ime()) ?: true
+
+            if (!isKeyboardOpen) {
+                focusManager.clearFocus()
+            }
+        }
+
+        viewTreeObserver.addOnGlobalLayoutListener(listener)
+        onDispose {
+            viewTreeObserver.removeOnGlobalLayoutListener(listener)
+        }
+    }
+
+    SearchBar(
+        query = searchText,
+        onQueryChange = onQueryChange,
+        onSearch = {
+            onQueryChange(it)
+            keyboardController?.hide()
+            focusManager.clearFocus()
+        },
+        active = false,
+        onActiveChange = { },
+        placeholder = { Text(text = stringResource(id = R.string.searchbar_hint)) },
+        leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = stringResource(id = R.string.desc_search)) },
+        trailingIcon = {
+            IconButton(onClick = onQueryClear) {
+                Icon(imageVector = Icons.Default.Close, contentDescription = stringResource(id = R.string.desc_clear))
+            }
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .focusable()
+    ) {
+    }
+}
+
--- presentation/src/main/java/com/dcs/presentation/ui/home/HomeViewModel.kt
+++ presentation/src/main/java/com/dcs/presentation/ui/home/HomeViewModel.kt
@@ -2,9 +2,23 @@ package com.dcs.presentation.ui.home
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
 
-) : ViewModel()
+) : ViewModel() {
+
+    private val _queryText = MutableStateFlow("")
+    val queryText = _queryText.asStateFlow()
+
+    fun onSearchTextChange(text: String) {
+        _queryText.value = text
+    }
+
+    fun onSearchTextClear() {
+        _queryText.value = ""
+    }
+}
