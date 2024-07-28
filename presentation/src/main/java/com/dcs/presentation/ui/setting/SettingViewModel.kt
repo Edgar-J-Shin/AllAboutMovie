c5f3aac8476d801a8d53414610ec2e2bedadd7a3
jay.lee <stkment@gmail.com>
2024-07-28 23:15:40 +0900

    feat #19 add SettingUiState

--- presentation/src/main/java/com/dcs/presentation/ui/setting/SettingViewModel.kt
+++ presentation/src/main/java/com/dcs/presentation/ui/setting/SettingViewModel.kt
@@ -2,8 +2,12 @@ package com.dcs.presentation.ui.setting
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.dcs.presentation.core.model.SettingUiState
+import com.dcs.presentation.core.model.UserProfile
+import com.dcs.presentation.core.state.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -13,6 +17,11 @@ class SettingViewModel @Inject constructor() : ViewModel() {
 
     private val _effect = MutableSharedFlow<SettingEffect>()
     val effect = _effect.asSharedFlow()
+
+    private val _state =
+        MutableStateFlow<UiState<SettingUiState>>(UiState.Loading)
+    val state = _state.asStateFlow()
+
 
     fun dispatch(event: SettingEvent) {
         when (event) {
