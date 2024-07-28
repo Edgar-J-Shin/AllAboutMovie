22c7e703ac5c2560a077092ade6ce51f61d4d46e
jay.lee <stkment@gmail.com>
2024-07-28 20:52:08 +0900

    feat #15 create SignInUseCase

---
+++ domain/src/main/java/com/dcs/domain/usecase/SignInUseCase.kt
@@ -0,0 +1,33 @@
+package com.dcs.domain.usecase
+
+import com.dcs.domain.model.RequestToken
+import com.dcs.domain.model.User
+import com.dcs.domain.repository.AuthRepository
+import dagger.Reusable
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+@Reusable
+class SignInUseCase @Inject constructor(
+    private val repo: AuthRepository,
+) {
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    operator fun invoke(requestToken: RequestToken): Flow<User> {
+        return repo.createSessionId(
+            requestToken = requestToken
+        )
+            .flatMapLatest { sessionId ->
+                repo.signIn(
+                    sessionId = sessionId,
+                    accountId = null
+                )
+            }
+            .onEach {
+                // TODO: Save user to local storage
+            }
+    }
+}
