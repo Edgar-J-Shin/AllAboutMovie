ae128587ef914ffeaa2184b142a7c28fd4df462a
jay.lee <stkment@gmail.com>
2024-07-28 22:58:49 +0900

    feat #19 add user avatar image, name, log-in button

--- presentation/src/main/java/com/dcs/presentation/core/model/SettingUiState.kt
+++ presentation/src/main/java/com/dcs/presentation/core/model/SettingUiState.kt
@@ -1,7 +1,8 @@
 package com.dcs.presentation.core.model
 
 import androidx.compose.runtime.Stable
-import com.dcs.domain.model.User
+import com.dcs.domain.model.SessionId
+import com.dcs.presentation.BuildConfig
 
 @Stable
 data class SettingUiState(
@@ -11,7 +12,43 @@ data class SettingUiState(
 sealed interface UserProfile {
     data object NotLoggedIn : UserProfile
 
-    data class LoggedIn(
-        val user: User,
+    data class User(
+        val id: Int,
+        val includeAdult: Boolean,
+        val name: String,
+        val username: String,
+        val avatarImageUrl: String,
+        val sessionId: SessionId,
     ) : UserProfile
+
+    companion object {
+
+        /**
+         * Factory function to create [UserProfile] from [com.dcs.domain.model.User].
+         *
+         * Ref. Avatar image URL
+         * https://www.themoviedb.org/talk/6033eeaa20ecaf003e1928ef#603447190d2944003e59aea5
+         */
+        operator fun invoke(
+            user: com.dcs.domain.model.User?,
+        ): UserProfile {
+            return if (user != null) {
+                User(
+                    id = user.id,
+                    includeAdult = user.includeAdult,
+                    name = user.name,
+                    username = user.username,
+                    sessionId = user.sessionId,
+                    avatarImageUrl = if (user.avatar.tmdb.value != null) {
+
+                        "${BuildConfig.TMDB_IMAGE_URL}/w200${user.avatar.tmdb.value}"
+                    } else {
+                        "${BuildConfig.TMDB_GRAVATR_URL}${user.avatar.gravatar.value}.jpf?s=200"
+                    },
+                )
+            } else {
+                NotLoggedIn
+            }
+        }
+    }
 }
--- presentation/src/main/java/com/dcs/presentation/ui/setting/SettingScreen.kt
+++ presentation/src/main/java/com/dcs/presentation/ui/setting/SettingScreen.kt
@@ -1,19 +1,39 @@
 package com.dcs.presentation.ui.setting
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import com.dcs.presentation.R
 import com.dcs.presentation.core.extensions.collectAsEffect
+import com.dcs.presentation.core.model.SettingUiState
+import com.dcs.presentation.core.model.UserProfile
+import com.dcs.presentation.core.state.UiState
+import com.dcs.presentation.core.theme.Gray1
 import com.dcs.presentation.ui.Screen
 
 @Composable
@@ -22,6 +42,8 @@ fun SettingRoute(
     modifier: Modifier = Modifier,
     viewModel: SettingViewModel = hiltViewModel(),
 ) {
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
     viewModel.effect.collectAsEffect { effect ->
         when (effect) {
             SettingEffect.SignIn -> {
@@ -30,41 +52,100 @@ fun SettingRoute(
         }
     }
 
-    SettingScreen(
-        onSettingEvent = viewModel::dispatch,
-        modifier = modifier.fillMaxSize(),
-    )
+    when (state) {
+        is UiState.Loading -> {
+            // Loading screen
+        }
+
+        is UiState.Success -> {
+            val settingUiState = (state as UiState.Success<SettingUiState>).data
+            if (settingUiState != null) {
+                SettingScreen(
+                    uiState = settingUiState,
+                    onSettingEvent = viewModel::dispatch,
+                    modifier = modifier
+                        .padding(horizontal = 20.dp)
+                        .fillMaxSize(),
+                )
+            }
+        }
+
+        is UiState.Error -> {
+            // Error screen
+        }
+    }
 }
 
 @Composable
 fun SettingScreen(
+    uiState: SettingUiState,
     onSettingEvent: (SettingEvent) -> Unit,
     modifier: Modifier,
 ) {
     Column(
         modifier = modifier,
     ) {
-        SignInButton(
-            onClick = { onSettingEvent(SettingEvent.SignIn) },
-            modifier = Modifier.fillMaxWidth()
+        UserProfile(
+            userProfile = uiState.userProfile,
+            onSettingEvent = onSettingEvent,
+            modifier = Modifier
+                .fillMaxWidth(),
         )
     }
 }
 
+@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
-private fun SignInButton(
-    onClick: () -> Unit,
+private fun UserProfile(
+    userProfile: UserProfile,
+    onSettingEvent: (SettingEvent) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Column(
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
         modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+        // Avatar Image
+        GlideImage(
+            model = if (userProfile is UserProfile.User) userProfile.avatarImageUrl else R.drawable.ic_person_48,
+            contentDescription = stringResource(R.string.profile_image_content_description),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(60.dp)
+                .clip(CircleShape)
+                .border(3.dp, Color.LightGray, CircleShape)
+                .background(Gray1),
+        )
+
+        // Name
+        Text(
+            text = if (userProfile is UserProfile.User) userProfile.name else stringResource(R.string.ask_to_login),
+            fontSize = 16.sp,
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 20.dp),
+        )
+
+        // SignIn or SignOut Button
         Button(
             shape = RoundedCornerShape(8.dp),
-            onClick = onClick
+            onClick = {
+                if (userProfile is UserProfile.User) {
+                    onSettingEvent(SettingEvent.SignOut(userProfile.sessionId.value))
+                } else {
+                    onSettingEvent(SettingEvent.SignIn)
+                }
+            },
+            modifier = Modifier
+                .padding(start = 20.dp)
         ) {
-            Text(text = "로그인 하기")
+            Text(
+                text = if (userProfile is UserProfile.User) {
+                    stringResource(R.string.sign_out)
+                } else {
+                    stringResource(R.string.sign_in)
+                }
+            )
         }
     }
 }
@@ -73,6 +154,7 @@ private fun SignInButton(
 @Composable
 fun PreviewSettingScreen() {
     SettingScreen(
+        uiState = SettingUiState(UserProfile.NotLoggedIn),
         onSettingEvent = {},
         modifier = Modifier.fillMaxSize(),
     )
