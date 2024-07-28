2f81f34ca77fbefd36a46fb0c3215f9ed5688fba
jay.lee <stkment@gmail.com>
2024-07-28 21:25:20 +0900

    feat #19 create GetUserUseCase

---
+++ domain/src/main/java/com/dcs/domain/usecase/GetUser.kt
@@ -0,0 +1,16 @@
+package com.dcs.domain.usecase
+
+import com.dcs.domain.model.User
+import com.dcs.domain.repository.AuthRepository
+import dagger.Reusable
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+@Reusable
+class GetUser @Inject constructor(
+    private val repo: AuthRepository,
+) {
+    operator fun invoke(): Flow<User?> {
+        return repo.getUser()
+    }
+}
