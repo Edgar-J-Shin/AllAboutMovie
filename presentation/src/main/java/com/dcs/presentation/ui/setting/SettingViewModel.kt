0a6eb42301bdf3eb43415d1e59c46145d16e258f
jay.lee <stkment@gmail.com>
2024-07-28 23:59:45 +0900

    feat #19 fetch Logged User Info

--- presentation/src/main/java/com/dcs/presentation/ui/setting/SettingViewModel.kt
+++ presentation/src/main/java/com/dcs/presentation/ui/setting/SettingViewModel.kt
@@ -2,18 +2,27 @@ package com.dcs.presentation.ui.setting
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.dcs.domain.usecase.GetUserUseCase
 import com.dcs.presentation.core.model.SettingUiState
 import com.dcs.presentation.core.model.UserProfile
 import com.dcs.presentation.core.state.UiState
+import com.dcs.presentation.core.state.asUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SettingViewModel @Inject constructor() : ViewModel() {
+class SettingViewModel @Inject constructor(
+    private val getUserUseCase: GetUserUseCase,
+) : ViewModel() {
 
     private val _effect = MutableSharedFlow<SettingEffect>()
     val effect = _effect.asSharedFlow()
@@ -22,6 +31,23 @@ class SettingViewModel @Inject constructor() : ViewModel() {
         MutableStateFlow<UiState<SettingUiState>>(UiState.Loading)
     val state = _state.asStateFlow()
 
+    init {
+        checkIfUserLoggedIn()
+    }
+
+    private fun checkIfUserLoggedIn() {
+        viewModelScope.launch {
+            getUserUseCase()
+                .map { user ->
+                    UserProfile(user)
+                }
+                .asUiState()
+                .onEach {
+                    _state.update { it }
+                }
+                .launchIn(this)
+        }
+    }
 
     fun dispatch(event: SettingEvent) {
         when (event) {
