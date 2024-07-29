0be80ef95beec340fbe18bd65b275ae996cbe9ae
jay.lee <stkment@gmail.com>
2024-07-29 11:32:09 +0900

    feat #19 create RoomModule

---
+++ data/src/main/java/com/dcs/data/local/di/RoomModule.kt
@@ -0,0 +1,37 @@
+package com.dcs.data.local.di
+
+import android.content.Context
+import androidx.room.Room
+import com.dcs.data.BuildConfig
+import com.dcs.data.local.room.AllAboutMovieDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RoomModule {
+
+    @Provides
+    @Singleton
+    fun provideRoomDatabase(
+        @ApplicationContext context: Context,
+    ): AllAboutMovieDatabase {
+        return Room.databaseBuilder(
+            context,
+            AllAboutMovieDatabase::class.java,
+            BuildConfig.APP_DATABASE_NAME
+        )
+            .fallbackToDestructiveMigrationFrom()
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideUserEntityDao(
+        allAboutMovieDatabase: AllAboutMovieDatabase,
+    ) = allAboutMovieDatabase.userEntityDao()
+}
