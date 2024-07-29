31af12803b0b3a579e030045ec22ea11bab0c5d7
jay.lee <stkment@gmail.com>
2024-07-29 11:30:50 +0900

    feat #19 create UserEntity

---
+++ data/src/main/java/com/dcs/data/local/room/entity/UserEntity.kt
@@ -0,0 +1,41 @@
+package com.dcs.data.local.room.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users")
+data class UserEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long,
+
+    @ColumnInfo(name = "name")
+    val name: String,
+
+    @ColumnInfo(name = "user_name")
+    val username: String,
+
+    @ColumnInfo(name = "session_id")
+    val sessionId: String,
+
+    @ColumnInfo(name = "include_adult")
+    val includeAdult: Boolean,
+
+    @ColumnInfo(name = "iso_3166_1")
+    val iso31661: String,
+
+    @ColumnInfo(name = "iso_639_1")
+    val iso6391: String,
+
+    @Embedded
+    val avatar: Avatar,
+)
+
+data class Avatar(
+    @ColumnInfo(name = "gravatar_hash")
+    val gravatarHash: String,
+
+    @ColumnInfo(name = "tmdb_url")
+    val tmdbUrl: String?,
+)
