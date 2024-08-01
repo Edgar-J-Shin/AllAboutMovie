56131dd1b5a1938a23ed9bcb933687c36649643f
jay.lee <stkment@gmail.com>
2024-08-01 20:52:21 +0900

    feat #19 change the way to collect state

--- presentation/src/main/java/com/dcs/presentation/ui/setting/SettingViewModel.kt
+++ presentation/src/main/java/com/dcs/presentation/ui/setting/SettingViewModel.kt
@@ -15,10 +15,8 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onCompletion
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
@@ -48,10 +46,9 @@ class SettingViewModel @Inject constructor(
                     SettingUiState(UserProfile(user))
                 }
                 .asUiState()
-                .onEach { state ->
+                .collect { state ->
                     _state.update { state }
                 }
-                .launchIn(this)
         }
     }
 
