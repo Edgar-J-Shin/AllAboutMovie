6dec395cc13ee0640f73d31d2142b2ef06928ef1
edgarshin <edgar.shin@tosslab.com>
2024-08-06 03:26:40 +0900

    feat #7 BaseDao 추가 - insert, update, delete

--- data/src/main/java/com/dcs/data/local/room/dao/KeywordDao.kt
+++ data/src/main/java/com/dcs/data/local/room/dao/KeywordDao.kt
@@ -1,17 +1,12 @@
 package com.dcs.data.local.room.dao
 
 import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.dcs.data.local.room.entity.Keyword
 import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface KeywordDao {
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(keyword: Keyword): Long
+interface KeywordDao: BaseDao<Keyword> {
 
     @Query("SELECT * FROM keywords ")
     fun getKeywordAll(): Flow<Keyword>
