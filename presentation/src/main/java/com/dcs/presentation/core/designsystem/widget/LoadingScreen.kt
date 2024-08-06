35722677d427a5e296ddb0d5eaf673208a21a98b
jay.lee <stkment@gmail.com>
2024-08-06 23:21:08 +0900

    feat #33 rollback LoadingScreen

--- presentation/src/main/java/com/dcs/presentation/core/designsystem/widget/LoadingScreen.kt
+++ presentation/src/main/java/com/dcs/presentation/core/designsystem/widget/LoadingScreen.kt
@@ -6,40 +6,28 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
-import androidx.compose.ui.window.DialogProperties
 
 @Composable
 fun LoadingScreen(
     modifier: Modifier = Modifier,
     size: Dp = 64.dp,
-    dismissOnBackPress: Boolean = false,
-    dismissOnClickOutside: Boolean = false,
-    usePlatformDefaultWidth: Boolean = false,
 ) {
     Box(
+        contentAlignment = Alignment.Center,
         modifier = modifier
             .fillMaxSize(),
     ) {
-        Dialog(
-            onDismissRequest = {},
-            properties = DialogProperties(
-                dismissOnBackPress = dismissOnBackPress,
-                dismissOnClickOutside = dismissOnClickOutside,
-                usePlatformDefaultWidth = usePlatformDefaultWidth,
-            ),
-        ) {
-            CircularProgressIndicator(
-                modifier = Modifier
-                    .size(size),
-                color = MaterialTheme.colorScheme.secondary,
-                trackColor = MaterialTheme.colorScheme.surfaceVariant,
-            )
-        }
+        CircularProgressIndicator(
+            modifier = Modifier
+                .size(size),
+            color = MaterialTheme.colorScheme.secondary,
+            trackColor = MaterialTheme.colorScheme.surfaceVariant,
+        )
     }
 }
 
