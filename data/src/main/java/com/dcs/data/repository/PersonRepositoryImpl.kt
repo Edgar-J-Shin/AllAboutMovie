c84bb5f2f42b3964be118b96d12fe28fe4b7c3d8
jay.lee <stkment@gmail.com>
2024-09-09 20:48:40 +0900

    feat #58 코드 복구

--- data/src/main/java/com/dcs/data/repository/PersonRepositoryImpl.kt
+++ data/src/main/java/com/dcs/data/repository/PersonRepositoryImpl.kt
@@ -7,17 +7,13 @@ import com.dcs.data.di.IoDispatcher
 import com.dcs.data.model.mapper.toEntity
 import com.dcs.data.pagingsource.PopularPeoplePagingSource
 import com.dcs.data.remote.datasource.PersonRemoteDataSource
-import com.dcs.domain.model.KnownFor
 import com.dcs.domain.model.Person
 import com.dcs.domain.model.PersonDetail
 import com.dcs.domain.repository.PersonRepository
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class PersonRepositoryImpl @Inject constructor(
@@ -39,23 +35,16 @@ class PersonRepositoryImpl @Inject constructor(
         ).flow
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     override fun getPersonDetail(personId: Int): Flow<PersonDetail> =
         flow {
-            emit(remote.getPersonDetail(personId).getOrThrow())
+            val personDetailResponse = remote.getPersonDetail(personId).getOrThrow()
+            val knownFor = remote.getSearchPerson(personDetailResponse.name)
+                .getOrThrow()
+                .results
+                .flatMap { it.knownFor }
+                .map { it.toEntity() }
+            emit(personDetailResponse.toEntity(knownFor))
         }
-            .flatMapLatest { personDetailResponse ->
-                flow {
-                    emit(remote.getSearchPerson(personDetailResponse.name).getOrThrow())
-                }
-                    .map { searchPersonResponse ->
-                        val knownFor: List<KnownFor> =
-                            searchPersonResponse.results
-                                .flatMap { it.knownFor }
-                                .map { it.toEntity() }
-                        personDetailResponse.toEntity(knownFor)
-                    }
-            }
             .flowOn(ioDispatcher)
 
     companion object {
